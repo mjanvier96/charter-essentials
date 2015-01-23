4268805b4503dbc7d41f0c895dfcde81a3de3b00
Michael Janvier <janvier.michael@charterschool.org>
2015-01-22 21:06:17 -0500

    Base line

--- Charter Essentials/FirstViewController.swift
+++ Charter Essentials/FirstViewController.swift
@@ -10,9 +10,42 @@ import UIKit
 
 class FirstViewController: UIViewController {
 
+    @IBOutlet var counterDownTimer: UILabel!
+
+    
+    let date = NSDate()
+    let calendar = NSCalendar.currentCalendar()
+    
     override func viewDidLoad() {
+        counterDownTimer.text = "3:00"
+        let method : Selector = "updateTime"
+        var timer = NSTimer.scheduledTimerWithTimeInterval(0.01, target: self, selector: method, userInfo: nil, repeats: true)
+        
         super.viewDidLoad()
+        
+     
+        
+        
         // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    func updateTime(){
+        counterDownTimer.text = currentTime()
+    }
+
+    func currentTime() -> String {
+        let components = NSCalendar.currentCalendar().components(.CalendarUnitHour | .CalendarUnitMinute | .CalendarUnitSecond | .CalendarUnitNanosecond, fromDate: NSDate())
+        let hour = components.hour
+        let minutes = components.minute
+        let seconds = components.second
+        let nanoseconds = components.nanosecond
+        //return hour.description + ":" + minutes.description + ":" + seconds.description + ":" + nanoseconds.description
+        return NSDate(timeIntervalSince1970: nil).timeIntervalSinceDate(components)
+    }
+    
+    
+    func DisplayCurrentTime(){
+        counterDownTimer.text = TIME_ABSOLUTE.description
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +53,9 @@ class FirstViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    
+
+    
 
 }
 
---
+++ Charter Essentials/TimeStuff.swift
@@ -0,0 +1,9 @@
+//
+//  TimeStuff.swift
+//  Charter Essentials
+//
+//  Created by Michael Janvier on 1/22/15.
+//  Copyright (c) 2015 Ambiark. All rights reserved.
+//
+
+import Foundation
